ca38ea5c727557f7a48b64ea55b67533746504e9
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-16 22:24:04 +0900

    Title: 정수의 최솟값, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240316/정수의 최솟값/minimum-value-of-an-integer.java
+++ 240316/정수의 최솟값/minimum-value-of-an-integer.java
@@ -1,24 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int newprint(int n){
+    public static int newprint(int a, int b, int c){
 
-        int result = 0;
+        int min = 101;
 
-        for(int i = 1 ; i <= n; i++){
-            result += i;
+        int[] arr = new int[3]{a, b, c};
+
+        for(int i = 0 ; i < 3 ; i++){
+            if(min > arr[i])
+                min = arr[i];
         }
         
-        return result/10;
+        return min;
             
     }   
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        System.out.print(newprint(n));
+        System.out.print(newprint(a, b, c));
             
         
 
