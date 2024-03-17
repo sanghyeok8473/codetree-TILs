47ce29c66f7471540538a62e44a143c4e692adf2
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-17 13:16:36 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240317/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean existDay(int m, int d){
+        
+        if(m == 2){
+            if(d>=1 && d<=28)
+                return true;
+            else
+                return false;
+        }
+        else if(m<=7 && m>=1 && m%2 ==1){
+            if(d>=1 && d<=31)
+                return true;
+            else
+                return false;
+        }
+        else if(m<=7 && m>=1 && m%2 == 0){
+            if(d>=1 && d<=30)
+                return true;
+            else
+                return false;
+        }
+        else if(m>=8 && m<=12 && m%2 == 0){
+            if(d>=1 && d<=31)
+                return true;
+            else
+                return false;
+        }
+        else if(m>=8 && m<=12 && m%2 == 1){
+            if(d>=1 && d<=30)
+                return true;
+            else
+                return false;
+        }
+            
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int D = sc.nextInt();
+
+        if(existDay(M, D))
+            System.out.print("Yes");
+        else
+            System.out.print("No");
+    }
+}
