6b578ca4db5e2ec5a5772e7bccc1211096b42a6b
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-17 12:54:58 +0900

    Title: 함수를 이용한 온전수 판별, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240317/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
+++ 240317/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -1,7 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int cntOnjeon(int a, int b){
+    public static boolean Onjeon(int a){
+        if(a%2==0 || a%10==5 || (a%3==0 && a%9!=0)){
+                return false;
+        }
+
+        return true;
+
+    }
+
+    
+
+    
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // System.out.print(cntOnjeon(a, b));
+        int cnt = 0; 
+
+        for(int i = a ; i<= b; i++){
+            if(Onjeon(i))
+                cnt++;
+
+        }
+
+        System.out.print(cnt);
+
+
+
+
+    }
+
+    /*public static int cntOnjeon(int a, int b){
 
         int cnt = 0;
 
@@ -15,18 +51,5 @@ public class Main {
 
         return cnt;
 
-    }
-
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        System.out.print(cntOnjeon(a, b));
-
-
-
-    }
+    }*/
 }
