b96cd1e1d8974d8269434f6eca3aa07cc22ab8e6
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-17 11:52:13 +0900

    Title: 함수를 이용한 소수 판별, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240317/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean check(int n){
+
+        for(int i = 2 ; i < n ; i++){
+            if(n%i==0){
+                return false;
+            }  
+        }
+
+        return true;
+
+    }   
+
+    public static int newsum(int a, int b){
+
+        int sum= 0;
+
+        for( int i = a ; i <= b ; i++){
+            if(check(i)){
+                sum+=i;
+            }
+
+                
+        }
+            
+        return sum;
+    }   
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(newsum(a,b));
+
+    }
+}
