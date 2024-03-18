99405837c2c9e97710817bc6cc56e05760133cf6
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-18 13:22:43 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240318/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class intWrapper{
+
+    int value;
+
+    public intWrapper(int n){
+        this.value = n;
+    }
+}
+
+public class Main {
+    public static int N, cnt = 0;
+    public static Scanner sc = new Scanner(System.in);
+
+    public static int fibo(int n){
+        if(n < 1)
+            return 0;
+        if(n >= 1 && n <= 2)
+            return 1;
+        if(n>=3)
+            return fibo(n-2) + fibo(n-1);
+        
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        N = sc.nextInt();
+
+
+        System.out.print(fibo(N));
+    }
+}
