1741c42683508bdc4154ea8a378556e3861ab6bb
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-18 16:19:42 +0900

    Title: Factorial, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240318/Factorial/factorial.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class intWrapper{
+
+    int value;
+
+    public intWrapper(int n){
+        this.value = n;
+    }
+}
+
+public class Main {
+    public static int n;
+    public static Scanner sc = new Scanner(System.in);
+
+    public static int factorial(int n){
+        if(n<=1)
+            return 1;
+        
+        return factorial(n-1) * n
+    }
+
+
+    public static void main(String[] args) {
+        n = sc.nextInt();
+
+
+        System.out.print(factorial(N));
+    }
+}
