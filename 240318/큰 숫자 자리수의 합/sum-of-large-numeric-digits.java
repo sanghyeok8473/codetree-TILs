d7fd63041e6e09fdddfbed88ef83dac9bdf46ec2
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-18 17:46:46 +0900

    Title: 큰 숫자 자리수의 합, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240318/큰 숫자 자리수의 합/sum-of-large-numeric-digits.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class intWrapper{
+
+    int value;
+
+    public intWrapper(int n){
+        this.value = n;
+    }
+}
+
+public class Main {
+    public static int n;
+    public static Scanner sc = new Scanner(System.in);
+
+    public static int f(int n){
+        if(n < 10)
+            return n;
+        
+        return f(n/10) + n%10;
+    }
+
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+
+
+        System.out.print(f(a*b*c));
+    }
+}
