69e5b3008c710d86cca0c16e49c9b0891394273b
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-18 11:55:32 +0900

    Title: 절댓값으로 변경, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240318/절댓값으로 변경/find-the-absolute-value.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class intWrapper{
+
+    int value;
+
+    public intWrapper(int n){
+        this.value = n;
+    }
+}
+
+public class Main {
+
+    public static void jeol(int[] arr2){
+        for(int i = 0 ; i < arr2.length ; i++){
+            if(arr[i] < 0)
+                arr[i]*=-1
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for(int i = 0 ; i < N ; i++)
+            arr[i] = sc.nextInt();
+
+        jeol(arr);
+        
+        for(int i = 0 ; i < arr.length ; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+
+        
+    }
+}
