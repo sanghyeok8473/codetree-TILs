019dd7d86b0f217f926f1c5f82efc4effb717a8e
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-19 14:14:34 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240319/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
+++ 240319/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -11,6 +11,53 @@ class intWrapper{
 
 public class Main {
     public static int n;
+    public static int[] arr = new int[11];
+    public static Scanner sc = new Scanner(System.in);
+
+    public static int lcm(int a, int b){
+        int gcd = 1;
+
+        for(int i = 1 ; i <= Math.min(a, b) ; i++ ){
+            if(a%i == 0 && b%i == 0)
+                gcd = i;
+        }
+
+        return (a*b)/gcd;
+    }  
+
+    public static int getlcm(int idx){
+        if(idx == 1)
+            return arr[1];
+
+        return lcm(getlcm(idx-1), arr[idx]);
+    }
+
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+
+        for(int i = 1 ; i <= n ; i++ )
+            arr[i] = sc.nextInt();
+
+        
+
+        System.out.print(getlcm(n));
+    }
+}
+
+/*import java.util.Scanner;
+
+class intWrapper{
+
+    int value;
+
+    public intWrapper(int n){
+        this.value = n;
+    }
+}
+
+public class Main {
+    public static int n;
     public static int allmul = 1;
     public static Scanner sc = new Scanner(System.in);
 
@@ -52,4 +99,4 @@ public class Main {
 
         System.out.print(lcm(arr, n-1));
     }
-}
+}*/
