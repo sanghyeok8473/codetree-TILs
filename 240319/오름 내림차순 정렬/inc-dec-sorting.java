0b9ff55d8046bdf0916b615113752cb2a649d33f
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-19 17:38:02 +0900

    Title: 오름 내림차순 정렬, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240319/오름 내림차순 정렬/inc-dec-sorting.java
+++ 240319/오름 내림차순 정렬/inc-dec-sorting.java
@@ -8,6 +8,7 @@ public class Main {
         int n = sc.nextInt();
 
         int[] arr = new int[n];
+        int[] arr2 = new int[n];
 
         for(int i = 0 ; i < n ; i++ )
             arr[i] = sc.nextInt();
@@ -20,6 +21,12 @@ public class Main {
         System.out.println();
 
         for(int i = n-1 ; i >= 0 ; i-- )
-            System.out.print(arr[i]+" ");
+            arr2[(n-1)-i] = arr[i];
+
+        for(int i = 0 ; i < n ; i++ )
+            System.out.print(arr2[i]+" ");
+
+        
+
     }
 }
