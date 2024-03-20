10b238fe46ef6a9b02ceded0f3b2975e9e1daddd
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-20 15:30:10 +0900

    Title: 코드네임, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240320/코드네임/code-name.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class codename {
+    char code;
+    int score;
+    
+    public codename(){
+        this.code= '0';
+        this.score = 0;
+    }
+
+    public codename(char code, int score){
+        this.code = code;
+        this.score = score;
+    }
+};
+
+public class Main {
+    public static int n;
+    public static int[] a  = new int[101];
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        codename[] fiveman = new codename[5];
+
+        int min = 101;
+        char min_code = '0';
+
+        for( int i = 0 ; i < 5 ; i++){   
+            char code = sc.next().charAt(0);
+            int  score= sc.nextInt();
+
+            fiveman[i] = new codename(code, score);
+            if(min > score){
+                min_code = fiveman[i].code;
+                min = fiveman[i].score;
+            }
+        }
+
+        
+        System.out.print(min_code+" "+min);
+    }
+}
