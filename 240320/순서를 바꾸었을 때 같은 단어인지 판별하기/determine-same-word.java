63a8e8df0a1cc26edbbfe1290a1cef6034e316a9
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-20 14:12:23 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240320/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
+++ 240320/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
@@ -2,38 +2,25 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
-    public static String a;
-    public static String b;
-
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
 
-        a = sc.next();
-        b = sc.next();
+        String a = sc.next();
+        String b = sc.next();
 
-        char[] sorted_a = a.toCharArray();
-        char[] sorted_b = b.toCharArray();
+        char[] charA = a.toCharArray();
+        char[] charB = b.toCharArray();
 
-        Arrays.sort(sorted_a);
-        Arrays.sort(sorted_b);
+        Arrays.sort(charA);
+        Arrays.sort(charB);
 
-        int len_a = sorted_a.length;
-        int len_b = sorted_b.length;
+        String sorted_a = new String(charA);
+        String sorted_a = new String(charB);
 
-        if(len_a != len_b){
+        if(sorted_a.equals(sorted_b))
+            System.out.print("Yes");
+        else
             System.out.print("No");
-        }
-        else{
-            for(int i = 0 ; i < len_a ; i++){
-                if(sorted_a[i]!=sorted_b[i]){
-                    System.out.print("No");
-                    break;
-                }
-                else if(sorted_a[i]==sorted_b[i] && i==len_a-1)
-                    System.out.print("Yes");
-            }
-        }
     }
 }
