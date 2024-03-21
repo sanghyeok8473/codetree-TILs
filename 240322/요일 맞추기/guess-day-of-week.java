35062e5d8826a956519ab4fcba23d8cb5388f023
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-22 01:14:03 +0900

    Title: 요일 맞추기, Time: 191ms, Memory: 10MB, Status: Passed - CodeTree

--- 240322/요일 맞추기/guess-day-of-week.java
+++ 240322/요일 맞추기/guess-day-of-week.java
@@ -8,8 +8,8 @@ public class Main {
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-        //                          0     1      2     3       4      5      6      7
-        String[] day = new String[]{"", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        //                            0      1      2     3       4      5      6
+        String[] day = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
         int[] date = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         int elapsedDate1 = 0;
@@ -25,27 +25,12 @@ public class Main {
         }
         elapsedDate2 += d2;
 
-        int elapsed = elapsedDate1 - elapsedDate2;
-        if(elapsed < 0)
-            elapsed *= -1;
+        int elapsed = elapsedDate2 - elapsedDate1;
+        while(elapsed < 0)
+            elapsed += 7;
 
-        if( elapsedDate1 > elapsedDate2){
-            elapsed %= 7;
-
-            if(elapsed == 0)
-                System.out.print(day[1]);
-            else
-                System.out.print(day[8-elapsed]);
-        }
-        else if( elapsedDate1 < elapsedDate2){
-            elapsed %= 7;
-
-            if(elapsed == 0)
-                System.out.print(day[1]);
-            else
-                System.out.print(day[1+elapsed]);
-        }
-        else
-            System.out.print(day[1]);
+        elapsed %= 7;
+            
+        System.out.print(day[elapsed]);
     }
 }
