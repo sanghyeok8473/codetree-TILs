501e496397526eeaf389f8070cd0a6ea4e1a1e56
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-22 00:47:16 +0900

    Title: Date to Date, Time: 184ms, Memory: 10MB, Status: Passed - CodeTree

--- 240322/Date to Date/date-to-date.java
+++ 240322/Date to Date/date-to-date.java
@@ -9,25 +9,22 @@ public class Main {
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
 
-        int[] date = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int[] date = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         int elapsedDate1 = 0;
         int elapsedDate2 = 0;
 
-        for( int i = 0 ; i < m1 ; i++){
+        for( int i = 1 ; i < m1 ; i++){
             elapsedDate1 += date[i];
         }
         elapsedDate1 += d1;
 
-        for( int i = 0 ; i < m2 ; i++){
+        for( int i = 1 ; i < m2 ; i++){
             elapsedDate2 += date[i];
         }
         elapsedDate2 += d2;
 
-        int elapsedtime = elapsedDate2 - elapsedDate1;
-
-        if(elapsedtime == 0)
-            elapsedtime++;
+        int elapsedtime = elapsedDate2 - elapsedDate1 + 1;
 
         System.out.print(elapsedtime);
 
