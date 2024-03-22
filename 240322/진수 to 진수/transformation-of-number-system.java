84838e2c0195f6e38f0929e37e0854bc43459fa4
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-22 18:11:07 +0900

    Title: 진수 to 진수, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240322/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        String n  = sc.next();
+        int cnt = 0, num = 0;
+
+        for(int i = 0 ; i < n.length(); i++ ){
+            num = num*8 + (n.charAt(i) - '0');
+        } // a진수로 표현된 n을 10진수로 변환(num)
+        
+
+        int[] digits = new int[200];
+
+        while(true){
+            if( num < b ){
+                digits[cnt] = num;
+                break;
+            }
+
+            digits[cnt++] = num % b;
+            num /= b;
+
+        }
+        for(int i = cnt ; i >= 0 ; i--)
+            System.out.print(digits[i]);
+        
+        
+        
+            
+    }
+}
