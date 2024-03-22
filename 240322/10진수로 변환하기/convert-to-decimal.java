3b212adcf618ad427c048610e9a91939dfc536a1
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-22 18:02:10 +0900

    Title: 10진수로 변환하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240322/10진수로 변환하기/convert-to-decimal.java
+++ 240322/10진수로 변환하기/convert-to-decimal.java
@@ -4,32 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] digits = new int[8];
+        String digits = sc.next();
 
         int num = 0;
 
-        int n = sc.nextInt();
-
-        if( n == 1 )
-            System.out.print(1);
-        else if( n == 0 )
-            System.out.print(0);
-        else{
-            int i = 1;
-
-            while(true){
-                if(n%10 != 0)
-                    num += Math.pow(i, n%10);
-                n /= 10;
-                i*=2;
-
-                if(n<10){
-                    num += Math.pow(i, n%10);
-                    break;
-                }
-            }
-            System.out.print(num);
+        for(int i = 0 ; i < digits.length(); i++ ){
+            num = num*2 + (digits.charAt(i) - '0');
         }
+
+        System.out.print(num);
         
         
         
