777b5d3f403e568e073f06bb212236496a1eb2d9
KAU-ParkSangHyeok <94914828+KAU-ParkSangHyeok@users.noreply.github.com>
2024-03-27 01:30:36 +0900

    Title: 이상한 진수 2, Time: 106ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240327/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] number = new int[10];
+
+        String num = sc.next();
+        int len = num.length();
+
+        for(int i = 0 ; i < len ; i++)
+            number[i] = num.charAt(i) - '0'; // 정수형 배열에 2진법숫자를 집어넣음.
+        
+
+        
+
+        for(int i = 0 ; i < len ; i++){
+            if(number[i] == 0){
+                number[i] = 1;
+                break;
+            }
+        }
+
+        int sum = 0;
+
+        for(int i = 0 ; i < len ; i++){
+            sum = (sum*2) + number[i];
+        }
+
+        System.out.print(sum);
+    }
+}
