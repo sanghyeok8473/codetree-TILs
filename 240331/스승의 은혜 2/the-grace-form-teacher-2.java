f6b96cc6142035c0a2b961e9b797628f07e49ca1
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-03-31 17:43:23 +0900

    Title: 스승의 은혜 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240331/스승의 은혜 2/the-grace-form-teacher-2.java
+++ 240331/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
-    public static int n, b, nowWork, maxGift = Integer.MIN_VALUE, cnt = 0;
+    public static int n, b, nowCal, nowCnt = 0, maxCnt = Integer.MIN_VALUE;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -17,22 +17,26 @@ public class Main {
             pi[i] = sc.nextInt();
             
 
-        for(int i = 0 ; i < n ; i++){ // 반값으로 만들 가격.
+        for(int i = 0 ; i < n ; i++){ // 반값으로 만들 물건의 가격.
             for(int j = 0 ; j < n ; j++)
-                pi2[j] = pi[j];
+                pi2[j] = pi[j];            // 반값으로 만들어 개수를 세본 이후 원래대로 돌려야되기 때문에 똑같은 배열을 하나 만듬.
+            
             pi2[i] /= 2;
 
             Arrays.sort(pi2);           // 오름차순으로 정렬(많이 사려면 가격이 낮은 것 부터 사야하므로)
+            nowCal = b; nowCnt = 0;
 
             for(int j = 0 ; j < n ; j++){
-                b -= pi2[j];
+                nowCal -= pi2[j];
+
                 if(b>=0)
-                    cnt++;
+                    nowCnt++;
                 else
                     break;
             }
+            maxCnt = Math.max(maxCnt, nowCnt);
         }
-        System.out.print(cnt);
+        System.out.print(maxCnt);
 
 
     }
