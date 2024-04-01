ba367b2c701019a92ca7aa34a70f0a0164bc5192
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-01 10:23:23 +0900

    Title: 흥미로운 숫자 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240401/흥미로운 숫자 2/interesting-numbers-2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int x, y, cnt = 0;
+
+    public static int checkNums(int x){ //자리수를 반환해주는 재귀함수
+        if(x < 10)
+            return 1;
+        
+        return checkNums(x/10) + 1;
+    }
+    public static boolean checkInterest(int x){
+        int nums = checkNums(x);
+        int[] nowNums = new int[nums + 1];
+        for(int i = 1 ; i < nums ; i++){
+            int div = Math.pow(10,nums-i);
+            nowNums[i] = x/div;
+            x -= nowNums[i]*div;
+        }
+        nowNums[nums] = x;
+        int cnt = 0, check = nowNums[1];
+        for(int i = 1 ; i <= nums ; i++){
+            if(nowNums[i] != check)
+                cnt++;
+        }
+        if(cnt == 1 || cnt == nums-1)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        x = sc.nextInt(); y = sc.nextInt();
+
+        for(int i = x ; i <= y ; i++){
+            if(checkInterest(i))
+                cnt++;
+        }
+        System.out.print(cnt);
+    }
+}
