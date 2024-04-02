d22b1379b3cea8e910def88900223bf0589cd41a
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-03 02:43:27 +0900

    Title: 이상한 폭탄 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240403/이상한 폭탄 3/strange-bomb-3.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static int n, k, max = -1;
+    public static int[] bombs = new int[100];
+    public static int[] checkBoom = new int[100];
+    public static int[] alreadyBoom = new int[100];
+
+    // 한개일 때는 무조건 차가 0이므로 최대 숫자의 개수는 최소 1
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt(); k = sc.nextInt(); 
+
+        for(int i = 0 ; i < n ; i++){
+            bombs[i] = sc.nextInt();
+            max = Math.max(bombs[i], max);
+        }
+            
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(j==i || Math.abs(j-i) > k)
+                    continue;
+                if(bombs[j] == bombs[i]){
+                    if(alreadyBoom[i] == 0){
+                        checkBoom[bombs[i]]++;
+                        alreadyBoom[i] = 1;
+                    }
+                    if(alreadyBoom[j] == 0){
+                        checkBoom[bombs[i]]++;
+                        alreadyBoom[j] = 1;
+                    }
+                }
+            }
+        }
+        
+        int maxCnt = 0, maxNum = 0;
+        for(int i = 0 ; i <= max ; i++){
+            if(checkBoom[i] > maxCnt){
+                maxNum = i;
+                maxCnt = checkBoom[i];
+            }
+            else if(checkBoom[i] == maxCnt){
+                maxNum = i;
+            }
+        }
+
+        System.out.print(maxNum);
+    }
+}
