a090f4f46f6a0a51a32eb2f6147342b3cc1ee537
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-03 05:29:01 +0900

    Title: 숫자들의 최대 차, Time: 269ms, Memory: 13MB, Status: Passed - CodeTree

--- 240403/숫자들의 최대 차/maximum-difference-in-numbers.java
+++ 240403/숫자들의 최대 차/maximum-difference-in-numbers.java
@@ -2,21 +2,17 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
-    public static int n, k, maxCnt = 1;
+    public static int n, k, cnt, max = 1;
     public static int[] nums = new int[1000];
 
-    public static int returnMaxMinDiff(int x, int y){
-        int max = 0, min = 10001;
-
-        for(int i = x ; i <= y ; i++){
-            max = Math.max(max, nums[i]);
-            min = Math.min(min, nums[i]);
+    public static boolean arrayExist(int x){
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] == x)
+                return true;
         }
-
-        return max - min;
-
+        return false;
     }
-    // 한개일 때는 무조건 차가 0이므로 최대 숫자의 개수는 최소 1
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -24,23 +20,20 @@ public class Main {
 
         for(int i = 0 ; i < n ; i++)
             nums[i] = sc.nextInt();
-
-        Arrays.sort(nums);
-        for(int ans = 2 ; ans <= n ; ans++){      // 고르는 숫자의 개수
-            boolean exist;
-            for(int i = 0 ; i < n - ans ; i++){     // 처음 고르는 숫자
-                exist = false;
-                for(int j = i+1 ; j < i+ans ; j++){   // 개수만큼 영역 선택
-                    if(returnMaxMinDiff(i, j) <= k){
-                        exist = true;
-                        break;
-                    }
-                }
-                if(exist)
-                    maxCnt = ans;
+        
+        for(int min = 1 ; min <= 10000 ; min++){ // 미리 생각해보는 최소값. 이 최소값이 일단 먼저 배열에 존재해야함. 존재할 때 k범위 이내를 다합한 최대값을 구함.
+            if(!arrayExist(min)){
+                continue;
             }
+            cnt = 0;
+            for(int i = 0 ; i < n ; i++){
+                if(nums[i] >= min && nums[i] - min <= k)
+                    cnt++;
+            }
+            max = Math.max(cnt, max);
+            
         }
 
-        System.out.print(maxCnt);
+        System.out.print(max);
     }
 }
