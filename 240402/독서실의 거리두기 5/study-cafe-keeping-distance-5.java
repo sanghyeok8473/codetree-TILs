f796f60a94b2a7bcdd6f62338e89815c68733405
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-02 13:29:50 +0900

    Title: 독서실의 거리두기 5, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240402/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
+++ 240402/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
@@ -1,35 +1,38 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int n, maxDist = 0;
-    public static String seats;
+    public static int n, maxDist = 0, nowDiff, minDiff = 30;
+    public static int[] seats = new int[20];
 
     public static int nearest(int x){
-        int l = 0, r = 0;
-        for(int i = x+1 ; i < n ; i++){
-            if(seats.charAt(i)-'0' == 1){
-                r = i - x;
-                break;
+        int[] newSeats = new int[n];
+        for(int i = 0 ; i < n ; i++)
+            newSeats[i] = seats[i];
+        
+        newSeats[x] = 1;        // 가장 가까운 두사람의 거리를 구할 새로운 배치
+        
+        minDiff = 30;
+        for(int i = 0 ; i < n-1 ; i++){           // 첫 번째 점
+            for(int j = i+1 ; j < n ; j++){     // 두 번째 점
+                nowDiff = j-i;
+                if(newSeats[i] == 1 && newSeats[j] == 1)  //두 좌석이 1일때의 모든 거리를 구해서 최소값을 찾으면 그 값이 가장 가까운 사람의 거리임.
+                    minDiff = Math.min(minDiff, nowDiff);
             }
         }
-        for(int i = x-1 ; i >= 0 ; i--){
-            if(seats.charAt(i)-'0' == 1){
-                l = x - i;
-                break;
-            }
-        }
-
-        return Math.min(l, r);
+        return minDiff;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
-        seats = sc.next();
+        String seat = sc.next();
 
-        for(int i = 0 ; i < n ; i++){      // 시작위치
-            if(seats.charAt(i)-'0' == 1)
+        for(int i = 0 ; i < n ; i++)
+            seats[i] = seat.charAt(i)-'0'; // 정수 배열로 넣어주기
+
+        for(int i = 0 ; i < n ; i++){      // 앉을 위치
+            if(seats[i] == 1)
                 continue;
             maxDist = Math.max(maxDist, nearest(i));
         }
