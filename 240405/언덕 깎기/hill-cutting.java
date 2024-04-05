4e88b72d4ee575589c0b0b3ffdea01967364db5c
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-05 20:06:03 +0900

    Title: 언덕 깎기, Time: 215ms, Memory: 12MB, Status: Passed - CodeTree

--- 240405/언덕 깎기/hill-cutting.java
+++ 240405/언덕 깎기/hill-cutting.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
-    public static int n, max = 0, min = 100, minCost = Integer.MAX_VALUE;
+    public static int n, max = 0, min = 100, minCost = Integer.MAX_VALUE, totalCost, nowVal;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,19 +10,23 @@ public class Main {
         n = sc.nextInt();
         int[] hill = new int[n];
 
-        for(int i = 0 ; i < n ; i++)
+        for(int i = 0 ; i < n ; i++){
             hill[i] = sc.nextInt();
+            max = Math.max(max, hill[i]);
+            min = Math.min(min, hill[i]);
+        }
             
-        Arrays.sort(hill);
 
-        for(int i = 0 ; i < n ; i++){      // 범위의 최소값을 미리 지정
-            int totalCost = 0, nowVal = hill[i];
+        for(int nowVal = min ; nowVal <= max ; nowVal++){      // 범위의 최소값을 미리 지정
+            totalCost = 0;
             for(int j = 0 ; j < n ; j++){
                 int nowCost = 0;
-                    if(hill[j] > nowVal + 17)
-                        nowCost = hill[j]-(hill[i]+17);
-                    else if(hill[j] < nowVal)
-                        nowCost = nowVal-hill[j];
+                if(hill[j] >= nowVal && hill[j] <= nowVal + 17)
+                    continue;
+                else if(hill[j] > nowVal + 17)
+                    nowCost = hill[j]-(nowVal+17);
+                else if(hill[j] < nowVal)
+                    nowCost = nowVal-hill[j];
                 totalCost += (nowCost*nowCost);
             }
             minCost = Math.min(minCost, totalCost);
