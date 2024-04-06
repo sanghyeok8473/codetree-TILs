ef760494b63ba09df3f7eaf38adcefdb86e8b4d9
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-07 00:03:24 +0900

    Title: 전부 겹치는 선분, Time: 171ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240407/전부 겹치는 선분/overlapping-line-segments.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static int n, k, minCost = 10000*100;
+    public static int[] segX = new int[100];
+    public static int[] segY = new int[100];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        n = sc.nextInt();
+        
+        for(int i = 0 ; i < n ; i++){
+            segX[i] = sc.nextInt();
+            segY[i] = sc.nextInt();
+        }
+            
+
+        for(int i = 1 ; i <= 100 ; i++){
+            boolean exist = true;
+            for(int j = 0 ; j < n ; j++){
+                if(segX[j] > i || segY[j] < i){
+                    exist = false;
+                    break;
+                }
+            }
+            if(exist){
+                System.out.print("Yes");
+                break;
+            }
+        }
+        
+
+
+    }
+}
