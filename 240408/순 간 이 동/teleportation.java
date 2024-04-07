09b0aa73e5d960c6717f679a48bfc5b236dc88f5
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-08 04:39:39 +0900

    Title: 순 간 이 동, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240408/순 간 이 동/teleportation.java
+++ 240408/순 간 이 동/teleportation.java
@@ -9,8 +9,9 @@ public class Main {
         
         a = sc.nextInt(); b = sc.nextInt(); x = sc.nextInt(); y = sc.nextInt(); 
 
-        min = Math.min(min,(Math.abs(a-x)+Math.abs(b-y)));
-        min = Math.min(min,(Math.abs(a-y)+Math.abs(b-x)));
+        min = Math.min(min,(Math.abs(a-x)+Math.abs(b-y)));  // x로 가서 순간이동 후 이동
+        min = Math.min(min,(Math.abs(a-y)+Math.abs(b-x)));  // y로 가서 순간이동 후 이동
+        min = Math.min(min,(Math.abs(b-a)));                // 그냥 이동
 
         System.out.print(min);
         
