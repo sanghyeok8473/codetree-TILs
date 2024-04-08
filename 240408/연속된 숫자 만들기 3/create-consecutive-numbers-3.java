3fc85e28a373544768dbdb8fea876cce721b4ee0
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-08 15:04:10 +0900

    Title: 연속된 숫자 만들기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240408/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
+++ 240408/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
@@ -41,8 +41,7 @@ public class Main {
             }
             else if(two >= one){
                 men[0] = men[2] - 1;
-                cnt++;
-                continue;
+                cnt++;s
             }
         }
         
