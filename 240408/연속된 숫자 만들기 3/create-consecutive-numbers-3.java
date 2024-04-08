68f216d92a84cf1ed60fb3fa7d32860943ebc069
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-08 15:26:20 +0900

    Title: 연속된 숫자 만들기 3, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240408/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
+++ 240408/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
@@ -24,26 +24,19 @@ public class Main {
             System.exit(0);
         }
         // 간격이 더 작은쪽의 끝이 간격이 더 큰쪽의 끝-1로 이동하면됨.
-        while(true){
-            Arrays.sort(men);
+        Arrays.sort(men);
 
-            int one = men[1] - men[0];
-            int two = men[2] - men[1];
+        int one = men[1] - men[0];
+        int two = men[2] - men[1];
 
-            if(checkEnd()){
-                System.out.print(cnt);
-                break;
-            }
-            if(one > two){                      // one과 two가 둘다 1일순 없음(둘다 1이면 checkEnd에서 끝났을 것.)
-                men[2] = men[1] - 1;            // 또한 two는 2, 3도 아님( 2나 3이면 위에서 끝났을 것.) 즉 4이상임.
-                cnt++;
-                continue;
-            }
-            else if(two >= one){
-                men[0] = men[2] - 1;
-                cnt++;s
-            }
+        if(checkEnd()){
+            System.out.print(cnt);
         }
-        
+        if(one > two){                      // one과 two가 둘다 1일순 없음(둘다 1이면 checkEnd에서 끝났을 것.)
+            System.out.print(one-1);
+        }
+        else{
+            System.out.print(two-1);
+        }
     }
 }
