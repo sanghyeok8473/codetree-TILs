1c54b91c84fe22104e68270856a4f6ac26d46799
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-09 12:01:26 +0900

    Title: L, R 그리고 B, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240409/L, R 그리고 B/l-r-and-b.java
+++ 240409/L, R 그리고 B/l-r-and-b.java
@@ -24,7 +24,9 @@ public class Main {
                 }
             }
         }
-        if((lx == rx && rx == bx && ((ry < ly && ry > by) || (ry < by && ry > ry))) || (lx == rx && rx == bx) && ((rx < lx && rx > bx) || (rx < bx && rx > rx)))
+        if((lx == rx && rx == bx) && ((ry < ly && ry > by) || (ry < by && ry > ly)))
+            result = Math.abs(lx-bx)+Math.abs(ly-by)-1 + 2;
+        else if((ly == ry && ry == by) && ((rx < lx && rx > bx) || (rx < bx && rx > lx)))
             result = Math.abs(lx-bx)+Math.abs(ly-by)-1 + 2;
         else
             result = Math.abs(lx-bx)+Math.abs(ly-by)-1;
