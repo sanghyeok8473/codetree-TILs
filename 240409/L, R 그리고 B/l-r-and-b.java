ff6d4f3a7dbd781f36884b091215b42320e2fe96
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-09 11:21:35 +0900

    Title: L, R 그리고 B, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240409/L, R 그리고 B/l-r-and-b.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static int lx, ly, bx, by, rx, ry, result = 0;
+
+    public static char[][] board = new char[10][10];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        for(int i = 0 ; i < 10 ; i++){
+            String str = sc.next();
+            for(int j = 0 ; j < 10 ; j++){
+                board[i][j] = str.charAt(j);
+                if(board[i][j] == 'L'){
+                    lx = i ; ly = j;
+                }
+                if(board[i][j] == 'R'){
+                    rx = i ; ry = j;
+                }
+                if(board[i][j] == 'B'){
+                    bx = i ; by = j;
+                }
+            }
+        }
+        if((lx == rx && rx == bx) || (ly == ry && ry == by))
+            result = Math.abs(lx-bx)+Math.abs(ly-by)-1 + 2;
+        else
+            result = Math.abs(lx-bx)+Math.abs(ly-by)-1;
+
+        System.out.print(result);
+        
+    }
+}
