cb8a68fbe9d652ada068a28ccc7b5fbacef30909
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-10 02:53:46 +0900

    Title: 세 수의 최대 곱, Time: 843ms, Memory: 34MB, Status: Passed - CodeTree

--- 240410/세 수의 최대 곱/maximum-product-of-three-numbers.java
+++ 240410/세 수의 최대 곱/maximum-product-of-three-numbers.java
@@ -3,27 +3,46 @@ import java.util.Arrays;
 
 public class Main {
     public static int n, max = -1000000000;
-    public static int[] nums = new int[100000];
 
-    public static int triple(int a, int b, int c){
-        return a*b*c;
+    public static int compareMinus(int[] arr){
+        int minusTwo = 0, allPlus = 0;
+        minusTwo = arr[0] * arr[1] * arr[n-1];
+        allPlus = arr[n-1] * arr[n-2] * arr[n-3];
+
+        return Math.max(minusTwo, allPlus);
     }
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         n = sc.nextInt();
+        int[] nums = new int[n];
+        boolean allMinus = true;
+        boolean zeroExist = false;
 
-        for(int i = 0 ; i < n ; i++)
+        for(int i = 0 ; i < n ; i++){
             nums[i] = sc.nextInt();
-
-        for(int i = 0 ; i < n-2 ; i++){
-            for(int j = i+1 ; j < n-1 ; j++){
-                for(int k = j+1 ; k < n ; k++){
-                    max = Math.max(triple(nums[i], nums[j], nums[k]), max);
-                }
-            }
+            if(nums[i] > 0)
+                allMinus = false;
+            if(nums[i] == 0)
+                zeroExist = true;
         }
-        System.out.print(max);
+        Arrays.sort(nums);
+
+        // 하나라도 양수면, 답은 반드시 양수임.
+        // 모든 값이 음수일때만 절대값이 작은것 세개를 곱하면 됨.
+        // 모든 값이 음수일때 0이 있으면 답은 반드시 0임.
+        // 이외에는 양수3개 또는 양수1개에 음수2개를 선택
+        if(allMinus){
+            if(zeroExist){
+                System.out.print(0);
+                System.exit(0);
+            }
+            System.out.print(nums[n-1] * nums[n-2] * nums[n-3]);            // 정렬 했으므로
+            System.exit(0);
+        }
+        else{
+            System.out.print(compareMinus(nums));
+        }
     }
 }
