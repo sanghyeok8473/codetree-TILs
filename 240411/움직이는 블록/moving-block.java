c86b66d74c0267c4f02dfb581aa01c5e6bbcc885
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-11 21:37:19 +0900

    Title: 움직이는 블록, Time: 330ms, Memory: 20MB, Status: Passed - CodeTree

--- 240411/움직이는 블록/moving-block.java
+++ 240411/움직이는 블록/moving-block.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
-    public static int n, min = 10000, max = 1, finalMin = Integer.MAX_VALUE;
+    public static int n, min = 10000, max = 1, finalSum = 0, sum = 0;
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,14 +14,17 @@ public class Main {
             nums[i] = sc.nextInt();
             min = Math.min(nums[i], min);
             max = Math.max(nums[i], max);
+            sum += nums[i];
         }
         for(int i = min ; i <= max ; i++){  // 동일해지는 값을 미리 지정
-            int nowSum = 0;
-            for(int j = 0 ; j < n ; j++){
-                nowSum += Math.abs(nums[j] - i);
+            if(i*n == sum){
+                for(int j = 0 ; j < n ; j++){
+                    if(nums[j] > i)
+                        finalSum += nums[j]-i;
+                        
+                }
             }
-            finalMin = Math.min(nowSum, finalMin);
         }
-        System.out.print(finalMin/2);
+        System.out.print(finalSum);
     }
 }
