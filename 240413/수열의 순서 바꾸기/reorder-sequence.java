e015c75c0f186efca798d6c78eb2d9cf75a82336
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-13 18:31:31 +0900

    Title: 수열의 순서 바꾸기, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240413/수열의 순서 바꾸기/reorder-sequence.java
+++ 240413/수열의 순서 바꾸기/reorder-sequence.java
@@ -4,26 +4,8 @@ import java.util.Arrays;
 public class Main {
     public static int n, m, cnt = 0, now, nowChk, nowChk2;
     public static int[] nums = new int[100];
-
-    public static boolean check(int[] arr){
-        for(int i = 0 ; i < n ; i++){
-            if(arr[i] != i+1)
-                return false;
-        }
-        return true;
-    }
     
     public static void main(String[] args) {
-        // 매 턴마다 체크해주는 함수를 돌려서 함수가 true면 cnt를 출력하고 종료
-        // 가능하면 now가 본인이 위치할 수 있는 곳으로 가면 좋음.
-        // 만약 now가 n이면 배열의 끝으로 가면됨.
-        // 맨 앞에 있는 숫자는 now-1의 오른쪽으로 가는게 최선 만약 안되면(now가 1이면) 2의 왼쪽으로
-        // 만약 now가 1인데, 2의 idx가 1이면 3의 왼쪽으로 이동
-        // 만약 맨 앞에있는 숫자가 n이면, 
-        
-
-        // 
-        // 못 가는 경우 : 맨 앞에 있는 숫자가 1일 때
         Scanner sc = new Scanner(System.in);
         
         n = sc.nextInt();
@@ -32,15 +14,21 @@ public class Main {
             nums[i] = sc.nextInt();
         
 
-        for(int i = 0; i < n; i++) {
-            boolean flag = false;
-            for(int j = i; j < n - 1; j++) {
-                if(nums[j] > nums[j + 1]) {
-                    flag = true;
-                }
-            }
-            if(flag) cnt++;
-        }
-        System.out.print(cnt);
+        // 맨 뒤에 있는 숫자들이 
+        // 정렬된 상태로 가장 길게 놓여져 있는 것이 좋습니다.
+        // 예를 들어 1 3 6 5 2 4 7 라는 수열이 있다면
+        // 2 4 7은 이미 정렬되어 있으므로
+        // 앞에 있는 1 3 6 5만 각 위치에 잘 옮겨주면 됩니다.
+        // 따라서 4가 됩니다.
+
+        // 즉, 뒤에서부터 보며
+        // 정렬되어 있지 않은 순간을 잡아
+        // 그 앞에 있는 원소는 전부 옮겨주면 됩니다.
+
+        int idx = n - 2;
+        while(idx >= 0 && nums[idx] < nums[idx + 1])
+            idx--;
+
+        System.out.print(idx + 1);
     }
 }
