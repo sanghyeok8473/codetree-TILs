8395b71869bd771ec130a66f608ac4ad6fd2ddbb
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-22 15:17:11 +0900

    Title: 기수 정렬 구현, Time: 3081ms, Memory: 52MB, Status: Time Limit Exceed - CodeTree

--- 240422/기수 정렬 구현/implement-radix-sort.java
+++ 240422/기수 정렬 구현/implement-radix-sort.java
@@ -8,17 +8,6 @@ public class Main {
         return 1+returnK(x/10);
     }
 
-    public static int returnNow(int x, int k){   // 특정 자릿수 반환함수
-        int cnt = 0;
-
-        while(true){
-            if(cnt == k)
-                return x%10;
-            x/=10;
-            cnt++;
-        }
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -33,8 +22,9 @@ public class Main {
 
         for(int pos = 0 ; pos < k ; pos++){    // 일의 자리부터 비교
             int[][] new_arr = new int[10][n];                // 특정 자리수가 모두 같을때 최대 n개가 들어감.
+            int nowPos = (int)Math.pow(10,pos);
             for(int i = 0 ; i < n ; i++){
-                int nowNum = returnNow(arr[i], pos);
+                int nowNum = (arr[i] / nowPos )%10;
                 for(int j = 0 ; j < n ; j++){
                     if(new_arr[nowNum][j] == 0){
                         new_arr[nowNum][j] = arr[i];
