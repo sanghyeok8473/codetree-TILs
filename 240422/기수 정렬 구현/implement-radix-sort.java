f5035c358c8dc199713d08d7a7b7ffdb93388597
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-22 15:05:54 +0900

    Title: 기수 정렬 구현, Time: 3080ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 240422/기수 정렬 구현/implement-radix-sort.java
+++ 240422/기수 정렬 구현/implement-radix-sort.java
@@ -22,13 +22,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt(), k = 0;
+        int n = sc.nextInt(), k = 0, max = 0;
         int[] arr = new int[n];
 
         for(int i = 0 ; i < n ; i++){
             arr[i] = sc.nextInt();
-            k = Math.max(k, returnK(arr[i])); // 가장 큰 값의 자리수 파악
+            max = Math.max(max, arr[i]); // 가장 큰 값의 자리수 파악
         }
+        k = returnK(max);
 
         for(int pos = 0 ; pos < k ; pos++){    // 일의 자리부터 비교
             int[][] new_arr = new int[10][n];                // 특정 자리수가 모두 같을때 최대 n개가 들어감.
