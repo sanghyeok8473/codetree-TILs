a9a74d82ac2f22dacb76fc3ab267c30f9c8e6076
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-22 15:31:27 +0900

    Title: 기수 정렬 구현, Time: 1534ms, Memory: 51MB, Status: Passed - CodeTree

--- 240422/기수 정렬 구현/implement-radix-sort.java
+++ 240422/기수 정렬 구현/implement-radix-sort.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Main {
     public static int returnK(int x){   // 자릿수 반환함수
@@ -15,39 +15,31 @@ public class Main {
         int n = sc.nextInt(), k = 0, max = 0;
         int[] arr = new int[n];
 
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 0 ; i < n ; i++)
             arr[i] = sc.nextInt();
-            max = Math.max(max, arr[i]); // 가장 큰 값의 자리수 파악
-        }
-        k = returnK(max);
-
-        /*for(int pos = 0 ; pos < k ; pos++){    // 일의 자리부터 비교
-            int[][] new_arr = new int[10][n];                // 특정 자리수가 모두 같을때 최대 n개가 들어감.
+        
+        for(int pos = 0 ; pos < 6 ; pos++){    // 일의 자리부터 비교
+            ArrayList<Integer>[] new_arr = new ArrayList[10];
+            for(int i = 0 ; i < 10 ; i++)
+                new_arr[i] = new ArrayList<>();
+            
             int nowPos = (int)Math.pow(10,pos);
             for(int i = 0 ; i < n ; i++){
                 int nowNum = (arr[i] / nowPos )%10;          // 현재 위치의 숫자 추출
-                for(int j = 0 ; j < n ; j++){
-                    if(new_arr[nowNum][j] == 0){
-                        new_arr[nowNum][j] = arr[i];
-                        break;
-                    }
-                }
+                new_arr[nowNum].add(arr[i]);
             } // 여기까지 하면 해당 자리를 기준으로 기수 정렬 끝, 이제 arr을 업데이트 해줄 차례
+
             int idx = 0;
             for(int i = 0 ; i < 10 ; i++){
-                for(int j = 0 ; j < n ; j++){
-                    if(new_arr[i][j] == 0)
-                        break;
-                    else{
-                        arr[idx] = new_arr[i][j];
-                        idx++;
-                    }
+                for(int j = 0 ; j < new_arr[i].size() ; j++){
+                    arr[idx] = new_arr[i].get(j);
+                    idx++;
                 }
             }
-        }*/
+        }
 
 
-        Arrays.sort(arr);
+
         for(int i = 0 ; i < n ; i++)
             System.out.print(arr[i]+" ");
     }
