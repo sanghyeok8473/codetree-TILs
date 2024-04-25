87d9d23f805f678d6f6e31836b030b12fe74617a
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-04-25 18:20:30 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 118ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240425/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<Character> s = new Stack<>();
+        String str =sc.next();
+        
+        for(int i = 0 ; i < str.length() ; i++){
+            char now = str.charAt(i);
+            if(now == '('){
+                s.push(now);
+            }
+            else{
+                if(s.empty()){
+                    System.out.print("No");
+                    break;
+                }
+                s.pop();
+            }
+        }
+        if(s.size() > 0)
+            System.out.print("No");
+        else
+            System.out.print("Yes");
+        
+    }
+}
