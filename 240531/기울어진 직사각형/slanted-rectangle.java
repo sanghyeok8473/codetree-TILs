c996ddbae33a7c6acdb2fb16a68eb1f74d30a1b7
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-05-31 19:12:59 +0900

    Title: 기울어진 직사각형, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240531/기울어진 직사각형/slanted-rectangle.java
+++ 240531/기울어진 직사각형/slanted-rectangle.java
@@ -23,75 +23,75 @@ public class Main {
         
         for(int i = 2 ; i < n ; i++){           //i,j는 시작점.
             for(int j = 1 ; j < n-1 ; j++){
-                totalMax = Math.max(totalMax, returnSum(i, j));
+                totalMax = Math.max(totalMax, returnMax(i, j));
             }
         }
         System.out.print(totalMax);
     }
 
-    public static int returnSum (int i, int j){     // 시작점이 x,y인 좌표를 받으면 최대의 기울어진 직사각형을 만들어서 그 변의 합을 반환
-        int idx = 0, sum = 0;
-        int x = i, y = j;
+    public static int returnMax (int x, int y){     // 시작점이 x,y인 좌표를 받으면 최대의 기울어진 직사각형을 만들어서 그 변의 합을 반환
+        int max = 0;
 
-        while(true){
-            sum += board[x][y];
-            if(returnOut(x, y, 1)){
-                idx++; 
-                x += dx[idx]; y += dy[idx];
-                break;
-            }
-            x += dx[idx]; y += dy[idx]; // 2번 방향으로 한 칸 전진
+        for(int len13 = 1 ; len13 < n-1 ; len13++){
+            if(outRange(x-len13, y+len13))
+                continue;
+            for(int len24 = 1 ; len24 < n-1 ; len24++){
+                if(outRange(x-len13-len24, y+len13-len24))
+                    continue;
+                max = Math.max(max, returnSum(x, y, len13, len24));
+            }    
         }
 
-        while(true){
-            sum += board[x][y];
-            if(returnOut(x, y, 2)){
-                idx++; 
-                x += dx[idx]; y += dy[idx];
-                break;
-            }
-            x += dx[idx]; y += dy[idx]; // 3번 방향으로 한 칸 전진
-        }
+        
 
-        while(true){
-            sum += board[x][y];
-            if(returnOut(x, y, 3)){
-                idx++; 
-                x += dx[idx]; y += dy[idx];
-                break;
-            }
-            x += dx[idx]; y += dy[idx]; // 4번 방향으로 한 칸 전진
-        }
+       return max;
+    }
+    // 굳이 격자의 끝까지 가는 것 만이 최대 값이 나오는게 아님. 즉, 변의 길이는 격자의 끝이 닿을 때까지 가는 것이 아닌, 모든 경우의 수를 계산해야함.
+    // 직사각형 모양이므로, 1,3번과 2,4번의 길이는 같음. 즉 길이 변수는 총 2개면 충분.
+    // 길이의 최대값은 n-1. 만약 길이가 n이면 정사각형의 대각선 길이와 같으므로 절대 직사각형이 나올 수 없음
 
-        while(true){
-            if(x == i && y == j)
-                break;
-            if (x < 0 || x >= n || y < 0 || y >= n) {
+    public static int returnSum(int x1, int y1, int len1, int len2){      // 시작점과 두 변의 길이가 주어지면 합을 구하는 함수.
+        int sum = 0, idx = 0;
+        int x = x1, y = y1;
+
+        for(int i = 0 ; i < len1 ; i++){
+            if(outRange(x,y))
                 return 0;
-            }
             sum += board[x][y];
             x += dx[idx]; y += dy[idx];
         }
+        idx++;
 
-       return sum;
-    }
-
-    public static boolean returnOut(int x, int y, int k){
-        switch(k){
-            case 1:
-                if(x == 1 || y == n-1)
-                    return true;
-                break;
-            case 2:
-                if(x == 0 || y == 1)
-                    return true;
-                break;
-            case 3:
-                if(x == n-2 || y == 0)
-                    return true;
-                break;
+        for(int i = 0 ; i < len2 ; i++){
+            if(outRange(x,y))
+                return 0;
+            sum += board[x][y];
+            x += dx[idx]; y += dy[idx];
         }
+        idx++;
 
-        return false;
+        for(int i = 0 ; i < len1 ; i++){
+            if(outRange(x,y))
+                return 0;
+            sum += board[x][y];
+            x += dx[idx]; y += dy[idx];
+        }
+        idx++;
+
+        for(int i = 0 ; i < len1 ; i++){
+            if(outRange(x,y))
+                return 0;
+            if(x==x1 && y==y1)
+                break;
+            sum += board[x][y];
+            x += dx[idx]; y += dy[idx];
+        }
+        
+        return sum;
     }
+
+    public static boolean outRange(int x, int y){
+        return (x < 0 || x >= n || y < 0 || y >= n);
+    }
+    
 }
