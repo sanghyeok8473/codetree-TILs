d195af5c143c1291d804912718b2414010881df4
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-01 12:56:27 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240601/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
+++ 240601/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
@@ -5,9 +5,6 @@ public class Main {
     public static int[][] board;
     public static int totalMax = Integer.MIN_VALUE, n, m;
 
-    public static int[] dx = {-1, -1, +1, +1};
-    public static int[] dy = {+1, -1, -1, +1};
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
