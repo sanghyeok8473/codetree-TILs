8410f64ea3afdfb33933abf1cacadd710880d523
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-10 14:14:06 +0900

    Title: 벽이 있는 충돌 실험, Time: 3056ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

--- 240610/벽이 있는 충돌 실험/collision-experiment-with-wall.java
+++ 240610/벽이 있는 충돌 실험/collision-experiment-with-wall.java
@@ -65,7 +65,7 @@ public class Main {
                 marbles.add(nowMarble);
                 
             }
-            for(int time = 0 ; time < m*2 ; time ++){
+            for(int time = 0 ; time < n*2 ; time ++){
                 for(int j = 0 ; j < marbles.size() ; j++){
                     int[] nextXYDir = move(marbles.get(j).x, marbles.get(j).y, marbles.get(j).dir);
                     marbles.get(j).x = nextXYDir[0];
