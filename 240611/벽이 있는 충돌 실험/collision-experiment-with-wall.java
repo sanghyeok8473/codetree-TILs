63f2f5172d9fce5075a0e800770933bcd2d07b74
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-11 12:30:36 +0900

    Title: 벽이 있는 충돌 실험, Time: 3198ms, Memory: 97MB, Status: Time Limit Exceed - CodeTree

--- 240611/벽이 있는 충돌 실험/collision-experiment-with-wall.java
+++ 240611/벽이 있는 충돌 실험/collision-experiment-with-wall.java
@@ -1,46 +1,46 @@
 import java.util.*;
 
-class Marble implements Comparable<Marble>{
+class Marble {
     int x;
     int y;
     int dir;
 
-    public Marble(int x, int y, int dir){
+    public Marble(int x, int y, int dir) {
         this.x = x;
         this.y = y;
         this.dir = dir;
     }
 
     @Override
-    public int compareTo(Marble otherMarble) {
-        if(this.x != otherMarble.x)
-            return this.x - otherMarble.x;
-        return this.y - otherMarble.y;
+    public String toString() {
+        return "Marble(" + x + "," + y + "," + dir + ")";
     }
 };
 
 public class Main {
     public static int t, n, m;
     public static int[] answer;
-    public static int[][] count;
     public static int[] dx = { -1, 0, 1, 0 };
     public static int[] dy = { 0, 1, 0, -1 };
-                            // 상 우 하 좌
-                            // 반대방향으로 전환 dir = (dir+2)%4;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        t = sc.nextInt(); answer = new int[t];
-        for(int caseNum = 0 ; caseNum < t ; caseNum ++){
-            n = sc.nextInt(); m = sc.nextInt();
+        t = sc.nextInt();
+        answer = new int[t];
+
+        for (int caseNum = 0; caseNum < t; caseNum++) {
+            n = sc.nextInt();
+            m = sc.nextInt();
             ArrayList<Marble> marbles = new ArrayList<>();
-            for(int i = 0 ; i < m ; i++){
-                int nowX = sc.nextInt();
-                int nowY = sc.nextInt();
-                nowX--; nowY--;
+
+            for (int i = 0; i < m; i++) {
+                int nowX = sc.nextInt() - 1;
+                int nowY = sc.nextInt() - 1;
                 int nowDir = 0;
                 char nowD = sc.next().charAt(0);
-                switch(nowD){
+
+                switch (nowD) {
                     case 'L':
                         nowDir = 3;
                         break;
@@ -54,76 +54,59 @@ public class Main {
                         nowDir = 2;
                         break;
                 }
-                Marble nowMarble = new Marble(nowX, nowY, nowDir);
-                marbles.add(nowMarble);
+                marbles.add(new Marble(nowX, nowY, nowDir));
             }
-            for(int time = 0 ; time < n*2 ; time ++){ // 같은 방향을 쳐다보면서 되돌아오려면, n*2의 시간이 필요. 처음에는 반드시 구슬이 겹치지 않게 주어짐.
-                count = new int[n][n];                 // 이동 후의 구슬 위치를 기록해줄 배열 선언.
-                for(int j = 0 ; j < marbles.size() ; j++){
-                    int[] nextXYDir = move(marbles.get(j).x, marbles.get(j).y, marbles.get(j).dir);
-                    marbles.get(j).x = nextXYDir[0];
-                    marbles.get(j).y = nextXYDir[1];
-                    marbles.get(j).dir = nextXYDir[2];
+
+            for (int time = 0; time < n * 2; time++) {
+                HashMap<String, Integer> positionCount = new HashMap<>();
+                HashMap<String, Marble> newMarbles = new HashMap<>();
+
+                for (Marble marble : marbles) {
+                    int[] nextXYDir = move(marble.x, marble.y, marble.dir);
+                    String key = nextXYDir[0] + "," + nextXYDir[1];
+
+                    if (positionCount.containsKey(key)) {
+                        positionCount.put(key, positionCount.get(key) + 1);
+                    } else {
+                        positionCount.put(key, 1);
+                        newMarbles.put(key, new Marble(nextXYDir[0], nextXYDir[1], nextXYDir[2]));
+                    }
                 }
-                Collections.sort(marbles);
-                for(int k = 0 ; k < marbles.size()-1 ; k++){
-                    if(marbles.get(k).x == marbles.get(k+1).x && marbles.get(k).y == marbles.get(k+1).y){
-                        marbles.remove(k); marbles.remove(k);
+
+                marbles.clear();
+                for (String key : newMarbles.keySet()) {
+                    if (positionCount.get(key) == 1) {
+                        marbles.add(newMarbles.get(key));
                     }
                 }
             }
+
             answer[caseNum] = marbles.size();
         }
-        /*Collections.sort(marbles);
-        for(int i = 0 ; i < m ; i++){
-            System.out.println(marbles.get(i).x + " " + marbles.get(i).y + " " + marbles.get(i).dir);
-        }*/
 
-        for(int ans : answer)
+        for (int ans : answer) {
             System.out.println(ans);
-        // 정답 출력
+        }
     }
 
-    public static int[] move(int x, int y, int dir){
+    public static int[] move(int x, int y, int dir) {
         int[] temp = new int[3];
-        switch(dir){
-            case 0: // 상
-                if(rangeValid(x-1, y)){
-                    temp[0] = x-1; temp[1] = y; temp[2] = dir;
-                }
-                else{
-                    temp[0] = x; temp[1] = y; temp[2] = (dir+2)%4;    // 방향만 반대로
-                }
-                break;
-            case 1: // 우
-                if(rangeValid(x, y+1)){
-                    temp[0] = x; temp[1] = y+1; temp[2] = dir;
-                }
-                else{
-                    temp[0] = x; temp[1] = y; temp[2] = (dir+2)%4;    // 방향만 반대로
-                }
-                break;
-            case 2: // 하
-                if(rangeValid(x+1, y)){
-                    temp[0] = x+1; temp[1] = y; temp[2] = dir;
-                }
-                else{
-                    temp[0] = x; temp[1] = y; temp[2] = (dir+2)%4;    // 방향만 반대로
-                }
-                break;
-            case 3: // 좌
-                if(rangeValid(x, y-1)){
-                    temp[0] = x; temp[1] = y-1; temp[2] = dir;
-                }
-                else{
-                    temp[0] = x; temp[1] = y; temp[2] = (dir+2)%4;    // 방향만 반대로
-                }
-                break;
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if (rangeValid(nx, ny)) {
+            temp[0] = nx;
+            temp[1] = ny;
+            temp[2] = dir;
+        } else {
+            temp[0] = x;
+            temp[1] = y;
+            temp[2] = (dir + 2) % 4;
         }
         return temp;
     }
 
-    public static boolean rangeValid(int r, int c){
+    public static boolean rangeValid(int r, int c) {
         return (r >= 0 && r < n && c >= 0 && c < n);
     }
 }
