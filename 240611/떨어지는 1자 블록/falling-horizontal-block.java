ddaf49e66f92b5bbda8b8c095e9a9f5474a9e774
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-11 16:26:13 +0900

    Title: 떨어지는 1자 블록, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240611/떨어지는 1자 블록/falling-horizontal-block.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    public static int n, m, k, first, end;
+    public static int[][] board;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        n = sc.nextInt(); m = sc.nextInt(); k = sc.nextInt(); k--;
+        first = k;  end = m + k - 1; // first는 블럭의 시작점, end는 블럭의 끝점
+        board = new int[n][n];
+        for(int i = 0 ; i < n ; i++)
+            for(int j = 0 ; j < n ; j++)
+                board[i][j] = sc.nextInt();
+        // 입력 파트
+
+        
+        move();
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void move(){
+        boolean none = true; // 1이 하나도 없음을 의미함.
+        for(int i = 1 ; i < n ; i++){
+            none = true;
+            for(int j = first ; j <= end ; j++){
+                if(board[i][j] == 1)
+                    none = false;
+            }
+            if(none == false){
+                for(int j = first ; j <= end ; j++){
+                    board[i-1][j] = 1;
+                }
+                break;
+            }
+        }
+    }
+}
