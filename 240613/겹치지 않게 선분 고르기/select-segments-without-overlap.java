0897927d6848b1f18503c65b00dec13939c8f08e
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-13 11:04:25 +0900

    Title: 겹치지 않게 선분 고르기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240613/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 240613/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -4,6 +4,8 @@ public class Main {
     public static int n, maxCnt = 1;
     public static int[] x1;
     public static int[] x2;
+    public static ArrayList<Integer> nowX1;
+    public static ArrayList<Integer> nowX2;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,20 +13,28 @@ public class Main {
         n = sc.nextInt(); x1 = new int[n]; x2 = new int[n];
 
         for(int i = 0 ; i < n ; i++){
-            int nowX1 = sc.nextInt();
-            int nowX2 = sc.nextInt();
-            x1[i] = nowX1;
-            x2[i] = nowX2;
+            int now1 = sc.nextInt();
+            int now2 = sc.nextInt();
+            x1[i] = now1;
+            x2[i] = now2;
         }
 
         for(int i = 0 ; i < n-1 ; i++){
-            int nowCnt = 1, minX = x1[i], maxX = x2[i];
+            int nowCnt = 1;
+            nowX1 = new ArrayList<>(); nowX2 = new ArrayList<>();
+            nowX1.add(x1[i]); nowX2.add(x2[i]); // 현재 선분을 일단 추가해둠. // nowX1과 nowX2에는, 겹치지 않는 선분들의 x1과 x2의 값들이 저장될 예정임.
             for(int j = i+1 ; j < n ; j++){
-                if(maxX < x1[j] || x2[j] < minX){       // 안 겹치면
+                boolean none = true;
+                for(int now = 0 ; now < nowX1.size() ; now++){ // 선분들의 모음에서 새로 추가될 수 있는 선분이 겹치지 않으려면, 모든 선분에 대해 겹치지 않으면 됨.
+                    if(nowX1.get(now) <= x2[j] || nowX2.get(now) >= x1[j])    // 현재 선분의 앞이 선분의 끝보다 뒤에 있거나, 현재 선분의 뒤가 선분의 앞보다 앞에 있으면 됨.
+                        none = false;                                   // 근데 생각해보니까 이러면 앞에서 엄청 큰 선분이 나와버리면 뒤에가 다 안들어가버리는 문제가 생김.
+                }                                                       // 결국 재귀로 하나하나 다 넣어보고 만족하는지 여부를 고르는 수 밖에 없겠네.. 아 못하겠는데 이거
+                if(none){
+                    nowX1.add(x1[j]);
+                    nowX2.add(x2[j]);
                     nowCnt++;
-                    minX = Math.min(minX, x1[j]);
-                    maxX = Math.max(maxX, x2[j]);
                 }
+                    
             }
             maxCnt = Math.max(maxCnt, nowCnt);
         }
