1fd74edf158ae77f78f6760b2c74e2393f78edbf
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-13 14:42:55 +0900

    Title: 겹치지 않게 선분 고르기, Time: 306ms, Memory: 22MB, Status: Wrong Answer - CodeTree

--- 240613/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 240613/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,11 +1,47 @@
 import java.util.*;
 
+class Pair{
+    int x1, x2;
+
+    public Pair(int x1, int x2){
+        this.x1 = x1;
+        this.x2 = x2;
+    }
+};
+
 public class Main {
-    public static int n, maxCnt = 1;
+    public static int n, maxCnt = 1, nowCnt;
     public static int[] x1;
     public static int[] x2;
-    public static ArrayList<Integer> nowX1;
-    public static ArrayList<Integer> nowX2;
+    public static ArrayList<Pair> x1x2;
+
+    public static boolean checkPossible(){  // 현재 배열이 모두 겹치지 않는 선분으로만 이루어져있는지 판단
+        for(int now = 0 ; now < x1x2.size() - 1 ; now++){
+            for(int next = now+1 ; next < x1x2.size() ; next++){
+                if(x1x2.get(now).x1 == x1x2.get(next).x1 && x1x2.get(now).x2 == x1x2.get(next).x2){ // 같은 선분을 넣은 조합이면 판단 X
+                    return false;
+                }
+                else if((x1x2.get(next).x1 >= x1x2.get(now).x1 && x1x2.get(next).x1 <= x1x2.get(now).x2) || (x1x2.get(next).x2 >= x1x2.get(now).x1 && x1x2.get(next).x2 <= x1x2.get(now).x2)){    // 겹치는 조건 : 한 선분의 두 끝점 중 하나라도 다른 선분의 사이에 들어가면 겹침.
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    //해결 방법 : 재귀를 이용해 모든 선택의 가짓수를 만들고, 겹치는 부분이 하나도 없는지 체크. 없으면 그 size를 Math.max에 넣어 최대값을 구함.
+    public static void findMaxcnt(int cnt){
+        if(cnt == nowCnt){
+            if(checkPossible())
+                maxCnt = Math.max(maxCnt, x1x2.size());
+            return;
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            x1x2.add(new Pair(x1[i], x2[i]));
+            findMaxcnt(cnt + 1);
+            x1x2.remove(x1x2.size() - 1);
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -18,26 +54,21 @@ public class Main {
             x1[i] = now1;
             x2[i] = now2;
         }
-
-        for(int i = 0 ; i < n-1 ; i++){
-            int nowCnt = 1;
-            nowX1 = new ArrayList<>(); nowX2 = new ArrayList<>();
-            nowX1.add(x1[i]); nowX2.add(x2[i]); // 현재 선분을 일단 추가해둠. // nowX1과 nowX2에는, 겹치지 않는 선분들의 x1과 x2의 값들이 저장될 예정임.
-            for(int j = i+1 ; j < n ; j++){
-                boolean none = true;
-                for(int now = 0 ; now < nowX1.size() ; now++){ // 선분들의 모음에서 새로 추가될 수 있는 선분이 겹치지 않으려면, 모든 선분에 대해 겹치지 않으면 됨.
-                    if(nowX1.get(now) <= x2[j] || nowX2.get(now) >= x1[j])    // 현재 선분의 앞이 선분의 끝보다 뒤에 있거나, 현재 선분의 뒤가 선분의 앞보다 앞에 있으면 됨.
-                        none = false;                                   // 근데 생각해보니까 이러면 앞에서 엄청 큰 선분이 나와버리면 뒤에가 다 안들어가버리는 문제가 생김.
-                }                                                       // 결국 재귀로 하나하나 다 넣어보고 만족하는지 여부를 고르는 수 밖에 없겠네.. 아 못하겠는데 이거
-                if(none){
-                    nowX1.add(x1[j]);
-                    nowX2.add(x2[j]);
-                    nowCnt++;
-                }
-                    
-            }
-            maxCnt = Math.max(maxCnt, nowCnt);
+        if(n==1){
+            System.out.print(1);
+            System.exit(0);
+        }
+        x1x2 = new ArrayList<>();
+        for(nowCnt = 2 ; nowCnt <= n ; nowCnt++){    // findMaxCnt : cnt라는 숫자를 주었을때, cnt만큼의 개수를 가지는 조합을 만들어보고, 겹치지 않는지 판단함.
+            findMaxcnt(0);
         }
         System.out.print(maxCnt);
+    }
+
+    public static void print(){
+        for(int i = 0 ; i < x1x2.size() ; i++){
+            System.out.println(x1x2.get(i).x1 + " " + x1x2.get(i).x2);
+        }
+        System.out.println();
     }
 }
