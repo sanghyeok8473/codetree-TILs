3d500229c71b7e84a6343f75264705f16dab246e
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-25 00:46:56 +0900

    Title: 그 요일은, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240625/그 요일은/the-day-of-the-day.py
+++ 240625/그 요일은/the-day-of-the-day.py
@@ -2,7 +2,7 @@ m1, d1, m2, d2 = map(int, input().split())
 a = input()
 
 date = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # 각 월의 날짜를 미리 기록
-day = ['', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] # 요일 
+day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'] # 요일 
 a = day.index(a) # 입력받은 요일을 day의 인덱스로 바꾸기(Mon이면 0, Sat이면 5)
 
 def numOfDays(m, d):
@@ -12,6 +12,14 @@ def numOfDays(m, d):
     returnDate += d
     return returnDate
 
+start = numOfDays(m1, d1)
+end = numOfDays(m2, d2)
+cnt = 0
+
+for i in range(start, end + 1):
+    if(i % 7 == a):
+        cnt += 1
+"""
 day_diff = numOfDays(m2, d2) - numOfDays(m1, d1)
 cnt = day_diff // 7 ## 일단 이 만큼은 등장했고, 남은 일수동안 요일이 한 번 더 있는지 여부를 확인하면 됨
 
@@ -20,5 +28,6 @@ if end == 0:
     cnt += 1
 elif 1 <= a <= end:
     cnt += 1
+"""
 
 print(cnt)
