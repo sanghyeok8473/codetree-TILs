a4c6ef41c98e0b562f55ed0b11d3d11dfaa84cc5
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-25 01:05:11 +0900

    Title: 그 요일은, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240625/그 요일은/the-day-of-the-day.py
+++ 240625/그 요일은/the-day-of-the-day.py
@@ -2,7 +2,7 @@ m1, d1, m2, d2 = map(int, input().split())
 a = input()
 
 date = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # 각 월의 날짜를 미리 기록
-day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'] # 요일 
+day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'] # 요일, 1월 1일이 월요일이므로 Mon의 인덱스는 1
 a = day.index(a) # 입력받은 요일을 day의 인덱스로 바꾸기(Mon이면 0, Sat이면 5)
 
 def numOfDays(m, d):
