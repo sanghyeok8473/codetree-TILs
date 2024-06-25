71544bf40ef8f6c3754cb0151681e648f94c0e6f
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-25 17:41:18 +0900

    Title: 흰검 칠하기, Time: 269ms, Memory: 38MB, Status: Passed - CodeTree

--- 240625/흰검 칠하기/painting-white-black.py
+++ 240625/흰검 칠하기/painting-white-black.py
@@ -6,34 +6,28 @@ now = 1000 * 100
 
 segments = [tuple(input().split()) for _ in range(n)] # 이 때, x는 아직 문자열임.
 
-def printnow():
-    for i in range(100000 - 5, 100000 + 6):
-        print(board[i], end=" ")
-    print(now)
-
 for x, R in segments:
     x = int(x)
     if(R == 'L'): #  흰색으로 칠함
         left = now - x
         right = now
-        for i in range(left, right):
-            color[i][0] += 1
+        for i in range(right, left, -1):
+            color[i][0] += 1 # 흰색 칠한 횟수 추가
             if color[i][0] >= 2 and color[i][1] >= 2:
                 board[i] = 3 # 회색
             else:
                 board[i] = 1 # 흰색
-        now -= x
+        now -= x - 1
     else: # 검은색으로 칠함
         left = now
         right = now + x
         for i in range(left, right):
-            color[i][1] += 1
+            color[i][1] += 1 # 검은색 칠한 횟수 추가
             if color[i][0] >= 2 and color[i][1] >= 2:
                 board[i] = 3 # 회색
             else:
                 board[i] = 2 # 검은색
-        now += x
-
+        now += x - 1
 cnt = 0
 
 result = [0, 0, 0] # 각각 흰, 검, 회를 의미
