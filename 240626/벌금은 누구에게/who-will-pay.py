9d2d5ea599f7761476be8760822174ef902139ec
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-26 00:59:31 +0900

    Title: 벌금은 누구에게, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240626/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,15 @@
+n, m, k = map(int, input().split())
+
+arr = []
+students = [0] * (n+1)
+
+for _ in range(m):
+    arr.append(int(input()))
+
+for i in range(m):
+    students[arr[i]] += 1
+    if students[arr[i]] >= k:
+        print(arr[i])
+        break;
+    if(i == m-1)
+        print(-1)
