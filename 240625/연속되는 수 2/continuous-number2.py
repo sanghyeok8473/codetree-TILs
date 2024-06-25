a8da3745c63582fb44e13ae8c17d41e425b7b819
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-25 22:13:16 +0900

    Title: 연속되는 수 2, Time: 198ms, Memory: 25MB, Status: Passed - CodeTree

--- 240625/연속되는 수 2/continuous-number2.py
+++ 240625/연속되는 수 2/continuous-number2.py
@@ -7,8 +7,8 @@ for _ in range(n):
 
 maxCnt, cnt = 0, 0
 
-for i in range(n-1):
-    if arr[i] == arr[i+1]:
+for i in range(n):
+    if i >= 1 and arr[i] == arr[i-1]:
         cnt += 1
     else:
         cnt = 1
