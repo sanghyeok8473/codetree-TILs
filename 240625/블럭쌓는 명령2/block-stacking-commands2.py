23071a21b2c5d17ae7b7b688ab642c4ffa246375
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-25 15:44:10 +0900

    Title: 블럭쌓는 명령2, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240625/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+blocks = [0] * n # 0으로 초기화된 블럭배열 선언
+
+for _ in range(k):
+    nowA, nowB = map(int, input().split())
+    for i in range(nowA - 1, nowB):
+        blocks[i] += 1
+
+blocks.sort(reverse = True) # 내림차순으로 정렬
+
+print(blocks[0])
