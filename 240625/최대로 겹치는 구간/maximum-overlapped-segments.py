4dc8e4fd09c89a4c4055ae23199e61575b4b3cd8
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-25 15:50:21 +0900

    Title: 최대로 겹치는 구간, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240625/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240625/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -5,7 +5,7 @@ segments = [tuple(map(int, input().split())) for _ in range(n)]
 
 for x1, x2 in segments:
     x1 += 100
-    x2 += 99
+    x2 += 100
     for i in range(x1, x2):
         board[i] += 1
 
