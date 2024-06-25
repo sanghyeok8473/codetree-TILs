271b2adacb49623244bd74f8599233ced1506082
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-25 16:57:40 +0900

    Title: 왔다 갔던 구역 2, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240625/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240625/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,32 +1,39 @@
 n = int(input())
 board = [0] * 2001 # n = 100, x = 10, 모든 명령이 L일때 -1000이 이동할 수 있는 최소값이므로 + 1000을 해야함.
+# board[x]는 [x, x+1]을 지나간 횟수.
 now = 1000
 
 segments = [tuple(input().split()) for _ in range(n)] # 이 때, x는 아직 문자열임.
 
+def printnow():
+    for i in range(1000 -11 , 1000 + 2 + 1):
+        print(board[i], end = " ")
+    print(now)
+
 for x, R in segments:
+    x = int(x)
     if(R == 'L'):
-        for i in range(now, now - int(x) - 1, - 1):
+        left = now - x
+        right = now
+        for i in range(left, right):
             board[i] += 1
-        now -= int(x)
+        now -= x
+        
     else:
-        for i in range(now, now + int(x) + 1):
+        left = now
+        right = now + x
+        for i in range(left, right):
             board[i] += 1
-        now += int(x)
+        now += x
 
 cnt = 0
-k = 0
 
-while k < 2001:
-    if board[k] >= 2:
-        now = []
-        for j in range(k, 2001):
-            if board[j] < 2:
-                cnt += len(now) - 1
-                k = j - 1
-                break
-            now.append(board[j])
-    k += 1
+for i in range(2000):
+    if board[i] >= 2:
+        cnt += 1
 
 print(cnt)
-# print(now - 1000)
+
+
+
+printnow()
