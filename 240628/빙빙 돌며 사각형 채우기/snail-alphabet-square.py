fd1af09aa95d51d14dd9e3802b16efaf7a0d0021
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-28 20:05:51 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240628/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -0,0 +1,32 @@
+dx = [0, 1, 0, -1] # 우 하 좌 상
+dy = [1, 0, -1, 0] # 우 하 좌 상
+
+n, m = map(int, input().split())
+x, y, d = 0, 0, 0 # 시작 x좌표, y좌표, 방향, 시작 숫자 설정
+
+board = [[0] * m for _ in range(n)] # 보드판 초기화
+
+def inRange(a, b):
+    return (0 <= a < n and 0 <= b < m and board[a][b] == 0) # 갔던 곳을 다시 가면 안됨.
+
+now = 65
+
+for i in range(n * m):
+    if inRange(x + dx[d], y + dy[d]): # 나아갈 곳이 격자 안이면서 이전에 간적 없는 곳이라면
+        board[x][y] = chr(now)
+        now += 1
+        if(now > ord('Z')):
+            now = 65
+        x, y = x + dx[d], y + dy[d]
+    else: # 나아갈 곳이 격자 안이 아니거나 이미 방문해서 숫자를 기록한 곳이라면
+        d = (d + 1) % 4 # 방향을 오른쪽으로 90도 틀고
+        board[x][y] = chr(now)
+        now += 1
+        if(now > ord('Z')):
+            now = 65
+        x, y = x + dx[d], y + dy[d]
+    
+for i in range(n):
+    for j in range(m):
+        print(board[i][j], end = " ")
+    print()
