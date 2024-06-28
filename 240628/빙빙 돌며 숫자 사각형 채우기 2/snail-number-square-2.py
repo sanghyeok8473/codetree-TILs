3e6702e521f364bf7d583b8fc0d0a66f1995a739
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-06-28 19:55:52 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240628/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,24 @@
+dx = [1, 0, -1, 0] # 하 우 상 좌
+dy = [0, 1, 0, -1] # 하 우 상 좌
+
+n, m = map(int, input().split())
+x, y, d = 0, 0, 0 # 시작 x좌표, y좌표, 방향, 시작 숫자 설정
+
+board = [[0] * m for _ in range(n)] # 보드판 초기화
+
+def inRange(a, b):
+    return (0 <= a < n and 0 <= b < m and board[a][b] == 0) # 갔던 곳을 다시 가면 안됨.
+
+for num in range(1, n * m + 1):
+    if inRange(x + dx[d], y + dy[d]): # 나아갈 곳이 격자 안이면서 이전에 간적 없는 곳이라면
+        board[x][y] = num
+        x, y = x + dx[d], y + dy[d]
+    else: # 나아갈 곳이 격자 안이 아니거나 이미 방문해서 숫자를 기록한 곳이라면
+        d = (d + 1) % 4 # 방향을 오른쪽으로 90도 틀고
+        board[x][y] = num
+        x, y = x + dx[d], y + dy[d]
+    
+for i in range(n):
+    for j in range(m):
+        print(board[i][j], end = " ")
+    print()
