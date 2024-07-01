0b905723137642bf07c95376a1bd2979e12539bf
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-02 05:43:12 +0900

    Title: 가까운 숫자, Time: 5102ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

--- 240702/가까운 숫자/nearest-number.py
+++ 240702/가까운 숫자/nearest-number.py
@@ -2,12 +2,16 @@ from sortedcontainers import SortedSet
 
 n = int(input())
 arr = list(map(int, input().split()))
-s = SortedSet() # s는 각 점의 좌표를 저장하는 treeset
-ans = SortedSet() # ans는 각 점끼리의 거리들을 저장하는 treeset
-s.add(0)
 
+s = SortedSet() # s는 각 점들의 위치를 저장하는 treeset
+s.add(0)
 for el in arr:
     s.add(el)
+    min_val = 1000000
     for val in s:
-        ans.add(abs(el - val))
-    print(ans[1])
+        left_idx = s.bisect_right(val) # val보다 큰 최초의 위치를 찾는다.
+        right_idx = left_idx - 1 # 바로 직전의 위치를 구한다.
+        
+        if left_idx < len(s):
+            min_val = min(min_val, s[left_idx] - s[right_idx])
+    print(min_val)
