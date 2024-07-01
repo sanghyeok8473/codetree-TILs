b9bb0c6fe600c03fafb315670334f30672d4f755
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-01 18:53:06 +0900

    Title: 두 수의 합, Time: 5082ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 240701/두 수의 합/sum-of-two-num.py
+++ 240701/두 수의 합/sum-of-two-num.py
@@ -5,17 +5,12 @@ ans = 0
 nums = list(map(int, input().split()))
 
 for i in range(n):
-    if nums[i] in d:
-        d[nums[i]] += 1
-    else:
-        d[nums[i]] = 1
+    d[i + 1] = nums[i] # d[위치] = 값
 
-for keys in d:
-    d[keys] -= 1
-    if (k - keys) in d and d[(k - keys)] > 0: # k - key가 존재하면서, k - key의 값이 1개 이상 있어야함.
-        d[keys] -= 1
-        d[(k - keys)] -= 1
-        ans += 1
-    d[keys] += 1
+for key1 in d:
+    for key2 in d:
+        if k - d[key1] == d[key2] and key1 != key2:
+            ans += 1
+    
 
-print(ans)
+print(ans // 2)
