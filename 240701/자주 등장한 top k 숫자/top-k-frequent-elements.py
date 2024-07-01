54fad8d0f080458b897db747805b12b6182b6e3a
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-01 20:39:30 +0900

    Title: 자주 등장한 top k 숫자, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240701/자주 등장한 top k 숫자/top-k-frequent-elements.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().split())
+d = dict()
+
+ans = 0
+nums = list(map(int, input().split()))
+rank = [0] * k
+
+for el in nums:
+    if el in d:
+        d[el] += 1
+    else:
+        d[el] = 1
+
+ans = sorted(d.items(), key = lambda x : (-x[1], x[0]))
+
+for i in range(k):
+    print(ans[i][0] , end = " ")
