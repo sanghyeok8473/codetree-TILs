84d4dd31811e4a78b6420cd4bbb99bb40b529343
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-02 05:18:24 +0900

    Title: 최대 숫자 구하기, Time: 477ms, Memory: 38MB, Status: Passed - CodeTree

--- 240702/최대 숫자 구하기/find-maximum-number.py
+++ 240702/최대 숫자 구하기/find-maximum-number.py
@@ -3,10 +3,7 @@ from sortedcontainers import SortedSet
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-s = SortedSet()
-
-for i in range(1, m+1):
-    s.add(i)
+s = SortedSet(range(1 , m + 1))
 
 for el in arr:
     s.remove(el)
