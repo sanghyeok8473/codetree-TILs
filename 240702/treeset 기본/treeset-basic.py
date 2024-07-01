8befaa6b25cd1d45eb78760cbbb3070b0e26e207
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-02 02:38:27 +0900

    Title: treeset 기본, Time: 1055ms, Memory: 31MB, Status: Passed - CodeTree

--- 240702/treeset 기본/treeset-basic.py
+++ 240702/treeset 기본/treeset-basic.py
@@ -6,28 +6,28 @@ s = SortedSet()      # treeset
 for _ in range(n):
     order = input()
     if order.startswith("add"):
-        x = order.split()[1]
+        x = int(order.split()[1])
         if x not in s:
             s.add(x)
     elif order.startswith("remove"):
-        x = order.split()[1]
+        x = int(order.split()[1])
         if x in s:
             s.remove(x)
     elif order.startswith("find"):
-        x = order.split()[1]
+        x = int(order.split()[1])
         if x in s:
             print("true")
         else:
             print("false")
     elif order.startswith("lower_bound"):
-        x = order.split()[1]
+        x = int(order.split()[1])
         idx = s.bisect_left(x)
         if idx == len(s):
             print("None")
         else:
             print(s[idx])
     elif order.startswith("upper_bound"):
-        x = order.split()[1]
+        x = int(order.split()[1])
         idx = s.bisect_right(x)
         if idx == len(s):
             print("None")
