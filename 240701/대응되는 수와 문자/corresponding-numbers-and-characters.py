3633a42b9062c449ef1d75909b2cfe88e1eddbcd
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-01 18:22:48 +0900

    Title: 대응되는 수와 문자, Time: 611ms, Memory: 43MB, Status: Passed - CodeTree

--- 240701/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 240701/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -1,14 +1,16 @@
 n, m = map(int, input().split())
 d = {}
+reverse_d = {}
 max_val = 0
 
 for i in range(1, n + 1):
     now = input()
     d[now] = i
+    reverse_d[i] = now
 
 for _ in range(m):
     order = input()
     if order in d:
         print(d[order])
     else:
-        print(list(d.keys())[int(order) - 1])
+        print(reverse_d[int(order)])
