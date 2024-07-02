efe02a498f6a80ed7c39ddfe70672fb2cb1c031e
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-02 21:41:39 +0900

    Title: 자리 차지하기, Time: 995ms, Memory: 39MB, Status: Passed - CodeTree

--- 240702/자리 차지하기/take-place.py
+++ 240702/자리 차지하기/take-place.py
@@ -1,12 +1,35 @@
 from sortedcontainers import SortedSet
 
-n, m = map(int, input().split())
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
-s = SortedSet() # s는 각 점들의 위치를 저장하는 treeset
+# 1번부터 m번까지 전부 비어져 있으므로
+# 빈 위치를 treeset으로 관리합니다.
+seats = SortedSet(range(1, m + 1))
 
-for el in arr:
-    if s.bisect_left(el) < el: # el이 들어왔을때, 여유 공간이 1개라도 있으면
-        s.add(el)
+# 답을 구해줍니다.
+ans = 0
+
+# 순서대로 앉혀봅니다.
+# 최선의 자리 선택은
+# ai보다 같거나 작은 최대 위치에 자리배치를 하는 것입니다.
+for elem in arr:
+    # ai보다 큰 최초의 위치를 먼저 찾습니다.
+    idx = seats.bisect_right(elem)
     
-print(len(s))
+    # 만약 큰 최초의 위치가 
+    # 첫 번째 위치가 아니라면,
+    # 바로 전 위치가
+    # ai보다 같거나 작은 최대 위치가 구해지므로,
+    # 해당 위치에 사람을 앉혀줍니다.
+    if idx != 0:
+        idx -= 1
+        seats.remove(seats[idx])
+        
+        # 답을 갱신해줍니다.
+        ans += 1
+    else:
+        break
+
+print(ans)
