72017d2248d360ab453b5a9485643ad94ce19103
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-03 11:48:20 +0900

    Title: 그래프 탐색, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240703/그래프 탐색/graph-traversal.py
+++ 240703/그래프 탐색/graph-traversal.py
@@ -2,6 +2,7 @@ n, m = map(int, input().split())
 
 graph = [[0] * (n + 1) for _ in range(n + 1)]
 visited = [False for _ in range(n + 1)]
+vertex_cnt = 0
 
 for _ in range(m):
     x, y = map(int, input().split())
@@ -12,15 +13,11 @@ def dfs(vertex):
     for curr_v in range(1, n + 1):
         if graph[vertex][curr_v] and not visited[curr_v]:
             visited[curr_v] = True
+            vertex_cnt += 1
             dfs(curr_v)
 
 
 visited[1] = True
 dfs(1)
 
-ans = 0
-
-for boolean in visited:
-    ans = ans + 1 if boolean else ans
-
-print(ans - 1)
+print(vertex_cnt)
