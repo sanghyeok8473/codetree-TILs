7cb24c4a8a1eeb161716f78c206260f21baad602
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-04 18:04:37 +0900

    Title: 트리 간선의 길이, Time: 368ms, Memory: 77MB, Status: Passed - CodeTree

--- 240704/트리 간선의 길이/length-of-tree-trunk.py
+++ 240704/트리 간선의 길이/length-of-tree-trunk.py
@@ -2,8 +2,6 @@ import sys
 sys.setrecursionlimit(10000)
 
 n = int(input())
-
-root = set(range(1, n + 1))
 edges = [[] for _ in range(n + 1)]
 visited  = [False for _ in range(n + 1)]
 arr = list(
@@ -11,15 +9,10 @@ arr = list(
     for _ in range(n - 1)
 )
 dist = [0] * (n + 1)
-not_Tree = False
 
 for x, y, d in arr:
     edges[x].append((y, d))
     edges[y].append((x, d))
-    if y in root:
-        root.remove(y)
-
-root = root.pop() # 루트노드 구하기
 
 # 루트에서 가장 먼 점을 찾은 다음, 그 점에서 가장 먼 점을 찾으면 그 부분이 정답
 
