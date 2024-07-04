ab2029e87e6ee36a954ce3aee09d91deb43dc682
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-04 16:26:17 +0900

    Title: 트리 파악, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/트리 파악/identifying-the-tree.py
+++ 240704/트리 파악/identifying-the-tree.py
@@ -3,7 +3,8 @@ n = int(input())
 edges = [[] for _ in range(n + 1)]
 visited = [False for _ in range(n + 1)]
 dist = [0] * (n + 1) # 시작점부터 x까지의 거리
-ans = n - 1
+ans = 0
+is_Leaf = [True for _ in range(n + 1)]
 
 piece = [0] * (n - 1)
 
@@ -12,18 +13,35 @@ arr = [
     for _ in range(n-1)
 ]
 
-# 리프노드 : edges[x]가 비어있으면 리프노드
+# 리프노드 : edges[x]가 비어있으면 리프노드, 즉 2 ~ n의 숫자들 중 arr의 x에서 등장하지 않은 수
 # 이 문제는 리프노드들이 루트 노드까지 가는 거리들의 총합을 구한다음, 그 값이 짝수인지 홀수인지 구하면 된다.
 # 총합이 홀수이면 1, 총합이 짝수이면 0이다.
 # 노드가 3개면, 간선은 2개다.(무조건 간선의 수는 노드-1개)
-# 즉, n이 주어지면 간선의 개수를 n-1로 잡고, append 할 때 x가 1이 아니면 간선에서 -1을 해주면(리프노드가 아니게 되므로) 된다.
 
 for x, y in arr:
-    if x != 1 and len(edges[x]) == 0: # x가 루트노드가 아니고, 아직 한 번도 나온적이 없어 이전까지 리프노드인 상태였다면
-        ans - 1
     edges[x].append(y)
+    is_Leaf[x] = False
+    
 
-if ans%2 == 0:
+def dfs(x): # y에 도착하면 거리를 출력 후 종료
+    for y in edges[x]:
+        if not visited[y]: # y를 아직 방문한적이 없으면, x가 y의 부모이다.
+            visited[y] = True
+            dist[y] = dist[x] + 1
+            dfs(y) # dfs 진행
+
+visited[1] = True
+dfs(1)
+
+# dfs를 루트노드에서 한 번 진행하면 특정 노드부터 루트노드까지의 길이가 기록됨. 이후에는 리프노드의 dist만 전부 더해서 짝홀 여부 판단
+
+for i in range(2, n + 1):
+    if is_Leaf[i]:
+        ans += dist[i]
+
+print(ans)
+
+if ans % 2 == 0:
     print(0)
 else:
     print(1)
