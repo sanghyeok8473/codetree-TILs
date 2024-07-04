6715b41463bf1bc3d8cbffbe0887585ed7ce682a
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-04 18:21:05 +0900

    Title: 트리 최적의 노드, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240704/트리 최적의 노드/tree-optimal-node.py
+++ 240704/트리 최적의 노드/tree-optimal-node.py
@@ -15,18 +15,31 @@ for _ in range(n-1):
 # 1부터 n까지의 모든 노드를 dfs한다. dfs할 때마다 dist는 초기화한다. max(dist)의 최소값을 구한다.
 
 def dfs(x):
+    global max_dist, last_node
+
     for y in edges[x]:
         if not visited[y]:
             visited[y] = True
             dist[y] = dist[x] + 1
+
+            if dist[y] > max_dist:
+                max_dist = dist[y]
+                last_node = y
+
             dfs(y)
 
-for i in range(1, n + 1):
-    visited  = [False for _ in range(n + 1)]
-    dist = [0 for _ in range(n + 1)] # dist[x] : dfs한 거리로부터 x번 노드까지의 거리
+# 지름을 구한다음, (지름+1)/2를 하면 됨.
+max_dist = 0
+last_node = 0
 
-    visited[i] = True
-    dfs(i)
-    ans = min(ans, max(dist))
+visited[1] = True
+dfs(1)
 
-print(ans)
+visited  = [False for _ in range(n + 1)]
+dist = [0 for _ in range(n + 1)] # dist[x] : dfs한 거리로부터 x번 노드까지의 거리
+
+
+visited[last_node] = True
+dfs(last_node)
+
+print((max_dist + 1) // 2)
