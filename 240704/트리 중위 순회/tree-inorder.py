2cd7177c0fd5eb45e9dbe7236044c0de9429182e
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-04 19:32:47 +0900

    Title: 트리 중위 순회, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240704/트리 중위 순회/tree-inorder.py
@@ -0,0 +1,35 @@
+# 변수 선언 및 입력:
+k = int(input())
+
+n = 2**k - 1
+
+tree_num = [0] * (n + 1)
+cnt = 1
+
+# a라는 배열에 중위 순회 순서를 입력받습니다.
+a = [0] + list(map(int, input().split()))
+
+
+# 중위 순회를 진행하여 각 트리의 위치에 맞는 번호를 채워 넣습니다.
+def in_order(x):
+    global cnt
+
+    # n보다 큰 노드는 없습니다. 빠져나갑니다.
+    if x > n:
+        return
+
+    in_order(x * 2)
+    print("inorder x를 시행합니다", x, cnt)
+    tree_num[x] = a[cnt]
+    cnt += 1
+    in_order(x * 2 + 1)
+
+   
+# 중위 순회를 진행하여 각 트리의 위치에 맞는 번호를 채워 넣습니다.  
+in_order(1)
+
+# 트리의 구조에 맞게 출력을 진행합니다.
+for i in range(1, k + 1):
+    for j in range(2**(i-1), 2**i):
+        print(tree_num[j], end=" ")
+    print()
