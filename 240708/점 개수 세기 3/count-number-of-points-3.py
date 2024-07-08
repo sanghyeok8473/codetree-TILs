8a2e95b8fe99c7e7d13c30e1dc8c923654a44bba
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-08 15:58:47 +0900

    Title: 점 개수 세기 3, Time: 484ms, Memory: 42MB, Status: Passed - CodeTree

--- 240708/점 개수 세기 3/count-number-of-points-3.py
+++ 240708/점 개수 세기 3/count-number-of-points-3.py
@@ -5,7 +5,17 @@ n, q = map(int, input().split())
 arr = list(map(int, input().split()))
 nums = SortedSet(arr)
 
-ab = [list(map(int, input().split())) for _ in range(q)]
+mapper = dict()
+cnt = 1
+for num in nums:
+    mapper[num] = cnt
+    cnt += 1
+
+ab = [
+    list(map(int, input().split()))
+    for _ in range(q)
+]
 
 for a, b in ab:
-    print(nums.bisect_right(b) - nums.bisect_right(a) + 1)
+    new_a, new_b = mapper[a], mapper[b]
+    print(new_b - new_a + 1)
