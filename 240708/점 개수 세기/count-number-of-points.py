a5a102c52bb4259746940b569301eeec738d455f
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-08 16:22:08 +0900

    Title: 점 개수 세기, Time: 251ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240708/점 개수 세기/count-number-of-points.py
+++ 240708/점 개수 세기/count-number-of-points.py
@@ -3,12 +3,25 @@ from sortedcontainers import SortedSet
 n, q = map(int, input().split())
 
 arr = list(map(int, input().split()))
-nums = SortedSet(arr)
-
 ab = [tuple(map(int, input().split())) for _ in range(q)]
 
+nums = SortedSet(arr)
+
+mapper = dict()
+cnt = 1
+for num in nums:
+    mapper[num] = cnt
+    cnt += 1
+
+for a, b in ab:
+    new_a = nums.bisect_right(a)
+    new_b = nums.bisect_right(b) if b < nums[-1] else n - 1
+    print(new_b - new_a + 1)
+
+"""
 for a, b in ab:
     if a < nums[0]:
         print(nums.bisect_right(b) - nums.bisect_right(a))
     else:
         print(nums.bisect_right(b) - nums.bisect_right(a) + 1)
+"""
