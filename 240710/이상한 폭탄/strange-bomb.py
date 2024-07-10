8032a16eaac19275582b18972a6fcdb9b1b935d5
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-10 16:22:12 +0900

    Title: 이상한 폭탄, Time: 237ms, Memory: 84MB, Status: Passed - CodeTree

--- 240710/이상한 폭탄/strange-bomb.py
+++ 240710/이상한 폭탄/strange-bomb.py
@@ -8,23 +8,13 @@ arr = [
 R = [-1 for _ in range(n)] # R[x] : 오른쪽에서 R[x]가 나타나는 idx값
 last = [-1 for _ in range(1000001)] # last[x] : x가 마지막으로 확인된 값
 
-for i in range(n - 1, 0-1 -1):
+for i in range(n - 1, -1, -1):
     R[i] = last[arr[i]]
     last[arr[i]] = i
 
 ans = -1
 for i in range(n):
-    if R[i] != 1 and R[i] - i <= k:
+    if R[i] != -1 and R[i] - i <= k:
         ans = max(ans, arr[i])
 
 print(ans)
-
-"""
-ans = -1
-for i in range(n):
-    for j in range(n):
-        if arr[j] in R[i] and i != j:
-            ans = max(ans, arr[j])
-
-print(ans)
-"""
