1f1df514fccea7f119445b4e990de1616a4f265d
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-10 11:57:43 +0900

    Title: 별이 있는 숫자로만의 이동, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240710/별이 있는 숫자로만의 이동/move-to-numbers-with-star.py
@@ -0,0 +1,40 @@
+# 입력:
+n, k = map(int, input().split())
+
+arr = [
+    [0] * (n + 1)
+    for _ in range(n + 1)
+]
+
+for i in range(1, n + 1):
+    row = list(map(int, input().split()))
+    for j in range(1, n + 1):
+        arr[i][j] = row[j - 1]
+
+prefix_sum = [
+    [0] * (n + 1)
+    for _ in range(n + 1)
+]
+
+def in_range(x, y):
+    return(1 <= x <= n and 1 <= y <= n)
+
+def get_sum(x, y):
+    sum_val = 0
+    for i in range(x - k, x + k + 1):
+        for j in range(y - k, y + k + 1):
+            if in_range(i, j) and (abs(i - x) + abs(j - y) <= k):
+                sum_val += arr[i][j]
+    return sum_val
+
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        prefix_sum[i][j] = get_sum(i, j)
+
+print(max(prefix_sum))
+
+"""
+prefix[x][y]를 정의하는 방식을 바꿀 수 있다.  x,y에서 거리가 k이내인 점들의 합으로 정의를 하자.
+
+"""
