72c332f93dc85ddc055255f8cb6fd98dd668a6c3
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-16 13:54:39 +0900

    Title: 선분 위의 점, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240716/선분 위의 점/point-on-the-line-segment.py
@@ -0,0 +1,39 @@
+n, m = map(int, input().split())
+
+spots = [0] + list(map(int, input().split()))
+
+position = [
+    tuple(map(int, input().split()))
+    for _ in range(m)
+]
+
+# x1의 lower_bound x2의 lower_bound의 차이를 구하면 됨
+
+for x1, x2 in position:
+    left, right, res = 1, n, 0
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if spots[mid] >= x1:
+            res = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    result = res
+
+    left, right, res = 1, n, 0
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if spots[mid] >= x2:
+            res = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    result = abs(result - res)
+
+    print(result)
