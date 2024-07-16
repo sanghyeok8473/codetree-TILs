1f301521fcf7127aff3550c4cccdcaa39324e1ce
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-16 14:12:21 +0900

    Title: 선분 위의 점, Time: 332ms, Memory: 34MB, Status: Passed - CodeTree

--- 240716/선분 위의 점/point-on-the-line-segment.py
+++ 240716/선분 위의 점/point-on-the-line-segment.py
@@ -8,33 +8,32 @@ position = [
 ]
 
 # x1의 lower_bound x2의 upper_bound의 차이를 구하면 됨
-# 양쪽다 소속된 점이면 1을 더해주어야함
+
+spots.sort()
 
 for x1, x2 in position:
-    left, right, res = 1, n, n + 1
+    left, right, lb = 1, n, n + 1
 
     while left <= right:
         mid = (left + right) // 2
 
         if spots[mid] >= x1:
-            res = mid
+            lb = mid
             right = mid - 1
         else:
             left = mid + 1
-    
-    result = -res
 
-    left, right, res = 1, n, n + 1
+    left, right, up = 1, n, n + 1
 
     while left <= right:
         mid = (left + right) // 2
 
         if spots[mid] > x2:
-            res = mid
+            up = mid
             right = mid - 1
         else:
             left = mid + 1
 
-    result += res
+    result = up - lb
 
     print(result)
