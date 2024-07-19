2fd87d2a2d900cdff30f54812928c0eb78c8aa28
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-19 13:37:41 +0900

    Title: 문제 추천 시스템 1, Time: 1305ms, Memory: 38MB, Status: Passed - CodeTree

---
+++ 240719/문제 추천 시스템 1/problem-recommendation-system-1.py
@@ -0,0 +1,25 @@
+from sortedcontainers import SortedSet
+
+n = int(input())
+
+question = SortedSet()
+
+for _ in range(n):
+    P, L = map(int, input().split())
+    question.add((L, P)) # 난이도가 먼저 기준이 되므로 난이도부터 저장
+
+m = int(input())
+
+for _ in range(m):
+    order = input()
+    if order.startswith("rc"):
+        x = int(order.split()[1])
+        print(question[-1][1] if x == 1 else question[0][1])
+    elif order.startswith("ad"): 
+        _, p, l = order.split()
+        p, l = int(p), int(l)
+        question.add((l, p)) # 난이도가 먼저 기준이 되므로 난이도부터 저장
+    else: # sv
+        _, p, l = order.split()
+        p, l = int(p), int(l)
+        question.remove((l, p))
