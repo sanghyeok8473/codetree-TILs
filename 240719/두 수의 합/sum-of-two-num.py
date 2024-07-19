5427bbcb612d5f1cb9b6644d710b9868735e48a1
sanghyeok8473 <94914828+sanghyeok8473@users.noreply.github.com>
2024-07-19 12:07:08 +0900

    Title: 두 수의 합, Time: 178ms, Memory: 40MB, Status: Passed - CodeTree

---
+++ 240719/두 수의 합/sum-of-two-num.py
@@ -0,0 +1,21 @@
+n, k = map(int, input().split())
+d = {}
+
+ans = 0
+nums = list(map(int, input().split()))
+
+for i in range(n):
+    if nums[i] in d:
+        d[nums[i]].append(i + 1)
+    else:
+        d[nums[i]] = []
+        d[nums[i]].append(i + 1)
+
+for key in d:
+    if (k - key) in d:
+        if k == key * 2:
+            ans += len(d[key])*(len(d[key])-1) // 2
+        else:
+            ans += len(d[key])*len(d[k - key])
+            d[k - key] = []
+print(ans)
